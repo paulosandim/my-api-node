62c728649cba17887124a8761caadd34e255b6c3
Paulo Sandim <pauloedusandim@gmail.com>
2023-11-24 00:01:25 -0300

    conceitos de put, post, delete

--- src/index.js
+++ src/index.js
@@ -8,6 +8,30 @@ app.get('/projects', function(request, response) {
   ])
 })
 
+app.post('/projects', function(request, response) {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3'
+  ])
+})
+
+app.put('/projects/:id', function(request, response) {
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3'
+  ])
+})
+
+app.delete('/projects/:id', function(request, response) {
+  return response.json([
+    'Projeto 2',
+    'Projeto 3'
+  ])
+})
+
+
 app.listen(3000, () => {
   console.log('✨✨✨ Server started on port 3000!!✨✨✨')
 })
