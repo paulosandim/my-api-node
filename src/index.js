ad78459de7ce576a040500c1b725aa43bbd7c84c
Paulo Sandim <pauloedusandim@gmail.com>
2023-11-23 23:51:54 -0300

    nodemon e conceito get basico

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(request, response) {
-  return response.json({
-    message: 'Hello world!!'
-  })
+app.get('/projects', function(request, response) {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2'
+  ])
+})
+
+app.listen(3000, () => {
+  console.log('✨✨✨ Server started on port 3000!!✨✨✨')
 })
