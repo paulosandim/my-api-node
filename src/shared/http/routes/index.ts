9ed42ea482a0e549a453fbdd71205c7ac44f19cd
Paulo Sandim <pauloedusandim@gmail.com>
2023-11-29 20:12:45 -0300

    tratamento de erros

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
+import { AppError } from '@shared/errors/AppError'
 import { Router } from 'express'
 
 const routes = Router()
 
 routes.get('/', (request, response) => {
+  throw new AppError('Acesso negado! 😭', 401)
   return response.json({ message: 'Hello World! ✨' })
 })
 
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
 import 'dotenv/config'
-import express from 'express'
+import express, { Request, Response } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import { routes } from './routes'
+import { AppError } from '@shared/errors/AppError'
 
 const app = express()
 app.use(cors())
@@ -10,8 +11,18 @@ app.use(express.json())
 
 app.use(routes)
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello world!!' })
+app.use((error: Error, request: Request, response: Response) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+  console.log(error)
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
 })
 
 app.listen(process.env.PORT, () => {
