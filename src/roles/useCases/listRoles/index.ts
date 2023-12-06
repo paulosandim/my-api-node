24f722fee7765282c1a44af623fa5b8020da3627
Paulo Sandim <pauloedusandim@gmail.com>
2023-12-06 16:19:33 -0300

    aula 38 - aplicando singleton no rolesrepository

--- src/roles/useCases/listRoles/index.ts
+++ src/roles/useCases/listRoles/index.ts
@@ -2,6 +2,6 @@ import { RolesRepository } from '@roles/repositories/RolesRepository'
 import { ListRolesUseCase } from './ListRolesUseCase'
 import { ListRolesController } from './ListRolesController'
 
-const rolesRespository = new RolesRepository()
+const rolesRespository = RolesRepository.getInstance()
 const listRolesUseCase = new ListRolesUseCase(rolesRespository)
 export const listRolesController = new ListRolesController(listRolesUseCase)
