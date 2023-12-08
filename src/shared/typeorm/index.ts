515ef3ef68a001a29f846d0a700b46ea5163fc4e
Paulo Sandim <pauloedusandim@gmail.com>
2023-12-08 01:32:33 -0300

    aula 45 - criando migration

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm'
+import { CreateRolesTable1702008270963 } from './migrations/1702008270963-CreateRolesTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1702008270963],
 })
