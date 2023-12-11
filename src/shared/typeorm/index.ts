2812f8cf8ff10d439100e9858f05b4090e681371
Paulo Sandim <pauloedusandim@gmail.com>
2023-12-11 17:52:15 -0300

    aula 46 - refatorando entidade de role

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1702008270963 } from './migrations/1702008270963-CreateRolesTable'
+import { Role } from '@roles/entities/Role'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [],
+  entities: [Role],
   migrations: [CreateRolesTable1702008270963],
 })
