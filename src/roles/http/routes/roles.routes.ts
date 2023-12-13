d9245ed19c2cfc3da8027223d3c650c2dc3be213
Paulo Sandim <pauloedusandim@gmail.com>
2023-12-13 15:30:57 -0300

    aula 49 - use case para buscar role por id

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,15 +1,18 @@
 import { createRolesController } from '@roles/useCases/createRole'
 import { listRolesController } from '@roles/useCases/listRoles'
+import { showRolesController } from '@roles/useCases/showRole'
 import { Router } from 'express'
 
 const rolesRouter = Router()
-
 rolesRouter.post('/', (request, response) => {
   return createRolesController.handle(request, response)
 })
-
 rolesRouter.get('/', (request, response) => {
   return listRolesController.handle(request, response)
+})
+
+rolesRouter.get('/:id', (request, response) => {
+  return showRolesController.handle(request, response)
 })
 
 export { rolesRouter }
