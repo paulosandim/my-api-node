e50a66c1d325f62319c01f74f8bf4ebb06fce484
Paulo Sandim <pauloedusandim@gmail.com>
2023-12-21 20:05:31 -0300

    aula 63 - refatoracao dos use case para usar container

--- src/roles/useCases/listRoles/index.ts
+++
@@ -1,7 +0,0 @@
-import { RolesRepository } from '@roles/repositories/RolesRepository'
-import { ListRolesUseCase } from './ListRolesUseCase'
-import { ListRolesController } from './ListRolesController'
-
-const rolesRespository = RolesRepository.getInstance()
-const listRolesUseCase = new ListRolesUseCase(rolesRespository)
-export const listRolesController = new ListRolesController(listRolesUseCase)
